5d33ed094e913718201b6bdc11f5e95ca73f62ae
SandeepSinghkaintura <https://sskaintura5550@gmail.com>
2024-09-23 14:40:40 +0530

    first commit

---
+++ src/component/TextArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/textArea.css';
+
+function TextArea() {
+    return (
+        <>
+            <div className='textarea' contenteditable="true"></div>
+        </>
+    )
+}
+
+export default TextArea; 
---
+++ src/pages/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TextArea from "../component/TextArea";
+
+const Hero = () => {
+    return (
+        <>
+            <h1 style={{ backgroundColor: 'lightblue', padding: '20px', textAlign: 'center' }}>Blog Writer: Write freely</h1 >
+            <TextArea />
+        </>
+    )
+}
+
+export default Hero
